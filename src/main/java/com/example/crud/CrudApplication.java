c59955aa77677feef04fbc3e39147348fbdfa2f6
OldMan35 <kostya_kv@mail.ru>
2021-06-08 00:05:19 +0300

    add postgresql

--- src/main/java/com/example/crud/CrudApplication.java
+++ src/main/java/com/example/crud/CrudApplication.java
@@ -3,7 +3,11 @@ package com.example.crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class CrudApplication {
@@ -17,5 +21,21 @@ public class CrudApplication {
     HiddenHttpMethodFilter hiddenHttpMethodFilter() {
 
         return new HiddenHttpMethodFilter();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/first_db");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("postgres");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
     }
 }
